b2bf2f8c329e42eea9c8c99f3e4bb9a2130eeb6b
syamgith <syamgithksudheesh@gmail.com>
2020-05-16 15:38:35 +0530

    design home

--- quizzy_flutter/lib/home.dart
+++ quizzy_flutter/lib/home.dart
@@ -5,22 +5,52 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text('Quizzy'),
-          SizedBox(height: 15.0),
-          MaterialButton(
-              color: Colors.red,
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        color: Color(0xffE41D15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
               child: Text(
-                'Play',
-                style: TextStyle(fontSize: 40),
+                'Quizzy',
+                style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 80.0,
+                    color: Colors.white,
+                    shadows: [
+                      Shadow(offset: Offset(9.0, 1.0), color: Colors.black)
+                    ]),
               ),
-              onPressed: () {})
-        ],
+            ),
+            Text(
+              "It's quiz time!!",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 120.0),
+            Container(
+              color: Colors.black,
+              child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(40),
+                    side: BorderSide(color: Colors.white),
+                  ),
+                  color: Colors.black,
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                  child: Text(
+                    'Play',
+                    style: TextStyle(fontSize: 30, color: Colors.white),
+                  ),
+                  onPressed: () {}),
+            )
+          ],
+        ),
       ),
     );
   }
