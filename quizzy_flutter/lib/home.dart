df5a75c578ada59702cd6096d18df6d7bb2dec8a
syamgith <syamgithksudheesh@gmail.com>
2020-05-16 18:34:02 +0530

    Ui modifications

--- quizzy_flutter/lib/home.dart
+++ quizzy_flutter/lib/home.dart
@@ -6,51 +6,73 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        color: Color(0xffE41D15),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(bottom: 20.0),
-              child: Text(
-                'Quizzy',
-                style: TextStyle(
-                    fontWeight: FontWeight.w700,
-                    fontSize: 80.0,
-                    color: Colors.white,
-                    shadows: [
-                      Shadow(offset: Offset(9.0, 1.0), color: Colors.black)
-                    ]),
+      backgroundColor: Colors.black,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.only(top: 100),
+              decoration: BoxDecoration(
+                color: Colors.pink,
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(200),
+                  bottomLeft: Radius.circular(200),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Qu!zzy',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 70.0,
+                        color: Colors.white,
+                        shadows: [
+                          Shadow(offset: Offset(9.0, 1.0), color: Colors.black)
+                        ]),
+                  ),
+                  SizedBox(height: 20.0),
+                  Text(
+                    "It's quiz time!!",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
               ),
             ),
-            Text(
-              "It's quiz time!!",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold),
+          ),
+          Expanded(
+            child: Container(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 130, vertical: 145),
+                child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      side: BorderSide(
+                        color: Colors.pink,
+                      ),
+                    ),
+                    color: Colors.white,
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                    child: Text(
+                      'Play',
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.black,
+                      ),
+                    ),
+                    onPressed: () {}),
+              ),
             ),
-            SizedBox(height: 120.0),
-            Container(
-              color: Colors.black,
-              child: RaisedButton(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(40),
-                    side: BorderSide(color: Colors.white),
-                  ),
-                  color: Colors.black,
-                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-                  child: Text(
-                    'Play',
-                    style: TextStyle(fontSize: 30, color: Colors.white),
-                  ),
-                  onPressed: () {}),
-            )
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
